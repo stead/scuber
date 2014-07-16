24bf19472eb89329844447eeec81d68a0304fc9e
abhishek0988 <abhishek@abhishek-mba.corp.dropbox.com>
2014-07-16 01:11:37 -0700

    Getting coordinates from contours
    contour_detect is now ready to be used as line detection algorithm!

--- tests/offset_detector.py
+++ tests/offset_detector.py
@@ -55,39 +55,35 @@ def contour_detect(file):
   # color filter (look for black)
   # imhsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
   # lower_black = numpy.array([0,0,0])
-  # upper_black = numpy.array([110,110,110])  # define range of blue color in HSV  
+  # upper_black = numpy.array([115,115,115])  # define range of blue color in HSV  
   # mask = cv2.inRange(imhsv, lower_black, upper_black) # Threshold the HSV image to get only black colors
-
   # show_img(mask, 1)
 
+  # thresholding
+  thresh = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+  _, thresh = cv2.threshold(thresh, 110, 255, cv2.THRESH_BINARY)  
+
   # edge detection
-  canny = cv2.Canny(im,180,220,apertureSize = 3)
+  canny = cv2.Canny(thresh,180,220,apertureSize = 3)
 
   # contour detection
-  contours, _ = cv2.findContours(canny,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
-  sorted_contours = sorted(contours, key=lambda x:len(x), reverse=True)
-  cv2.drawContours(im,sorted_contours[0:3],-1,(0,255,0),3)
+  contours, _ = cv2.findContours(canny,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+  # get contours sorted by length
+  sorted_contours = sorted(contours, key=lambda x:cv2.arcLength(x,False), reverse=True)
+  cv2.drawContours(im,sorted_contours[0:1],-1,(0,255,0),3) # draw longest contour
 
+  # get points for a single contour
   cnt = sorted_contours[0]
-  cv2.drawContours(im,[cnt],0,(0,255,0),3)
+  mask = numpy.zeros(im.shape,numpy.uint8)
+  cv2.drawContours(mask,[cnt],0,255,-1)
+  pixelpoints = numpy.transpose(numpy.nonzero(mask)) 
+
+  # Find x coordinates of endpoints
+  xTop = pixelpoints[0][1] # IMPORTANT: pixelpoints is returned in row, column format
+  xBottom = pixelpoints[-1][1]
+  print xTop,xBottom
 
   show_img(im, 1)
-
-
-  #contours, hierarchy = cv2.findContours(thresh,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
-
-  # sorted_contours = sorted(contours, key=lambda x:len(x), reverse=True)
-  # cnt = sorted_contours[0]
-  # cv2.drawContours(im,[cnt],0,(0,255,0),3)
-
-  #cv2.drawContours(im,contours,-1,(0,255,0),3)
-  #show_img(im, 0.5)
-  # k = cv2.waitKey(0)
-  # if k == ord('h'):
-  #   print cnt
-  # elif k == 27:
-  #   cv2.destroyAllWindows()
-  #   sys.exit(0)
 
 if __name__ == '__main__':
   path_to_images = "sample_images/highmount/"
